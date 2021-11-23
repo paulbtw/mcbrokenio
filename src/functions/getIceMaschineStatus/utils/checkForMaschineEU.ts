ed0e7af933dab3f851d4185e0fb1a8c37d609bc8
Paulbtw <p-p-v-2010@hotmail.de>
2021-11-23 14:28:00 +0100

    chore: update enum, change eu url

--- src/functions/getIceMaschineStatus/utils/checkForMaschineEU.ts
+++ src/functions/getIceMaschineStatus/utils/checkForMaschineEU.ts
@@ -17,7 +17,7 @@ export const checkForMaschineEU = async (
 ) => {
   try {
     const response = await axios.get(
-      `https://eu-prod.api.mcd.com/exp/v1/restaurant/${nationalStoreNumber}?filter=full&storeUniqueIdType=ECPID`,
+      `https://eu-prod.api.mcd.com/exp/v1/restaurant/${nationalStoreNumber}?filter=full&storeUniqueIdType=NatlStrNumber`,
       {
         headers: {
           authorization: `Bearer ${bearerToken}`,
