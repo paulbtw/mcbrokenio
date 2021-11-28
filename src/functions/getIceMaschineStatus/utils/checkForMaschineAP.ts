2306a69bc3ff187ff224706e02217fd5b981b2f7
paulbtw <paul.vatiche@gmail.com>
2021-11-28 17:32:18 +0100

    feat: add icemaschinestatus for australia

---
+++ src/functions/getIceMaschineStatus/utils/checkForMaschineAP.ts
@@ -0,0 +1,68 @@
+import { Logger } from '@sailplane/logger';
+import axios from 'axios';
+import { hasProduct } from '.';
+import {
+  Availability,
+  IceType,
+  IRestaurantInfoResponse,
+  Locations,
+} from '../../../types';
+
+const logger = new Logger('checkForMaschineAP');
+
+export const checkForMaschineAP = async (
+  bearerToken: string,
+  nationalStoreNumber: string,
+  location: Locations,
+  clientId: string | undefined,
+) => {
+  if (!clientId) {
+    throw Error('clientId is missing');
+  }
+
+  try {
+    const response = await axios.get(
+      `https://ap-prod.api.mcd.com/exp/v1/restaurant/${nationalStoreNumber}?filter=full&storeUniqueIdType=NSN`,
+      {
+        headers: {
+          authorization: `Bearer ${bearerToken}`,
+          'mcd-clientId': clientId,
+          'mcd-sourceapp': 'GMA',
+          'mcd-marketid': location,
+          'mcd-uuid': '"',
+          'accept-language': 'en-AU',
+        },
+      },
+    );
+
+    const data = response.data as IRestaurantInfoResponse;
+
+    let hasMilchshake: Availability = Availability.UNKNOWN;
+    let hasMcFlurry: Availability = Availability.UNKNOWN;
+    let hasMcSundae: Availability = Availability.UNKNOWN;
+
+    if (data.response?.restaurant?.catalog?.outageProductCodes) {
+      const { outageProductCodes } = data.response.restaurant.catalog;
+      hasMilchshake = hasProduct[IceType.MILCHSHAKE](
+        outageProductCodes,
+        location,
+      );
+      hasMcFlurry = hasProduct[IceType.MCFLURRY](outageProductCodes, location);
+      hasMcSundae = hasProduct[IceType.MCSUNDAE](outageProductCodes, location);
+    }
+    return {
+      hasMilchshake,
+      hasMcFlurry,
+      hasMcSundae,
+      status: data.response.restaurant.restaurantStatus ?? 'UNKNOWN',
+    };
+  } catch (error) {
+    logger.error(error);
+    return {
+      hasMilchshake: Availability.UNKNOWN,
+      hasMcSundae: Availability.UNKNOWN,
+      hasMcFlurry: Availability.UNKNOWN,
+      status: 'UNKNOWN',
+    };
+  }
+};
