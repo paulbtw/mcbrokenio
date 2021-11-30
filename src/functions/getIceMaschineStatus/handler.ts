01829f173b4a1ff6a850bd2e5415f3a801f06b34
paulbtw <paul.vatiche@gmail.com>
2021-11-30 14:16:05 +0100

    fix: error retry

--- src/functions/getIceMaschineStatus/handler.ts
+++ src/functions/getIceMaschineStatus/handler.ts
@@ -20,118 +20,124 @@ const logger = new Logger('getIceMaschineStatus');
 export const main: Handler = async (_, context) => {
   logger.debug(`Starting the Lambda. ID: ${context.awsRequestId}`);
 
-  logger.debug('Get new Bearer token');
-  const bearerTokenEU = await getNewBearerToken(APIType.EU);
-  logger.debug('new Token EU: ', bearerTokenEU);
+  try {
+    logger.debug('Get new Bearer token');
+    const bearerTokenEU = await getNewBearerToken(APIType.EU);
+    logger.debug('new Token EU: ', bearerTokenEU);
 
-  const bearerTokenEL = await getNewBearerToken(APIType.EL);
-  logger.debug('new Token EL: ', bearerTokenEL);
+    const bearerTokenEL = await getNewBearerToken(APIType.EL);
+    logger.debug('new Token EL: ', bearerTokenEL);
 
-  const bearerTokenUS = await getNewBearerToken(APIType.US);
-  logger.debug('new Token US: ', bearerTokenUS);
+    const bearerTokenUS = await getNewBearerToken(APIType.US);
+    logger.debug('new Token US: ', bearerTokenUS);
 
-  const bearerTokenAP = await getNewBearerToken(APIType.AP);
-  logger.debug('new Token AP: ', bearerTokenAP);
+    const bearerTokenAP = await getNewBearerToken(APIType.AP);
+    logger.debug('new Token AP: ', bearerTokenAP);
 
-  const clientIdEl = getClientId(BASIC_TOKEN_EL);
-  const clientIdUs = getClientId(BASIC_TOKEN_US);
-  const clientIdEu = getClientId(BASIC_TOKEN_EU);
-  const clientIdAp = getClientId(BASIC_TOKEN_AP);
+    const clientIdEl = getClientId(BASIC_TOKEN_EL);
+    const clientIdUs = getClientId(BASIC_TOKEN_US);
+    const clientIdEu = getClientId(BASIC_TOKEN_EU);
+    const clientIdAp = getClientId(BASIC_TOKEN_AP);
 
-  logger.debug('Ensure Database Connection');
-  await createDatabaseConnection();
+    logger.debug('Ensure Database Connection');
+    await createDatabaseConnection();
 
-  const posToCheck = await Pos.find({
-    where: { hasMobileOrdering: true },
-    order: { updatedAt: 'ASC' },
-    take: 2000,
-  });
+    const posToCheck = await Pos.find({
+      where: { hasMobileOrdering: true },
+      order: { updatedAt: 'ASC' },
+      take: 2000,
+    });
 
-  const now = new Date();
+    const now = new Date();
 
-  const newPosArray: Pos[] = [];
+    const newPosArray: Pos[] = [];
 
-  const batchedPos = chunk<Pos>(posToCheck, 10);
+    const batchedPos = chunk<Pos>(posToCheck, 10);
 
-  // eslint-disable-next-line no-restricted-syntax
-  for await (const posArray of batchedPos) {
-    await Promise.all(posArray.map(async (pos) => {
-      const newPos = pos;
-      logger.debug(`Checking Pos: ${pos.nationalStoreNumber}`);
-      const posId = newPos.nationalStoreNumber;
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const posArray of batchedPos) {
+      await Promise.all(
+        posArray.map(async (pos) => {
+          const newPos = pos;
+          logger.debug(`Checking Pos: ${pos.nationalStoreNumber}`);
+          const posId = newPos.nationalStoreNumber;
 
-      const countryInfo = CountryInfos[newPos.country];
+          const countryInfo = CountryInfos[newPos.country];
 
-      const storeApi = countryInfo.getStores.api;
-      let bearerToken = '';
-      if (storeApi === APIType.EU) {
-        bearerToken = bearerTokenEU;
-      } else if (storeApi === APIType.EL) {
-        bearerToken = bearerTokenEL;
-      } else if (storeApi === APIType.US) {
-        bearerToken = bearerTokenUS;
-      } else if (storeApi === APIType.AP) {
-        bearerToken = bearerTokenAP;
-      }
+          const storeApi = countryInfo.getStores.api;
+          let bearerToken = '';
+          if (storeApi === APIType.EU) {
+            bearerToken = bearerTokenEU;
+          } else if (storeApi === APIType.EL) {
+            bearerToken = bearerTokenEL;
+          } else if (storeApi === APIType.US) {
+            bearerToken = bearerTokenUS;
+          } else if (storeApi === APIType.AP) {
+            bearerToken = bearerTokenAP;
+          }
 
-      let clientId = '';
-      if (storeApi === APIType.EL) {
-        clientId = clientIdEl;
-      } else if (storeApi === APIType.US) {
-        clientId = clientIdUs;
-      } else if (storeApi === APIType.EU) {
-        clientId = clientIdEu;
-      } else if (storeApi === APIType.AP) {
-        clientId = clientIdAp;
-      }
+          let clientId = '';
+          if (storeApi === APIType.EL) {
+            clientId = clientIdEl;
+          } else if (storeApi === APIType.US) {
+            clientId = clientIdUs;
+          } else if (storeApi === APIType.EU) {
+            clientId = clientIdEu;
+          } else if (storeApi === APIType.AP) {
+            clientId = clientIdAp;
+          }
 
-      clientId = clientId.trim();
+          clientId = clientId.trim();
 
-      const { hasMilchshake, hasMcFlurry, hasMcSundae, status } =
-      await checkForMaschine[storeApi](
-        bearerToken,
-        `${posId}`,
-        newPos.country,
-        clientId,
+          const { hasMilchshake, hasMcFlurry, hasMcSundae, status } =
+            await checkForMaschine[storeApi](
+              bearerToken,
+              `${posId}`,
+              newPos.country,
+              clientId,
+            );
+
+          if (status === 'NOT CONNECTED') {
+            return;
+          }
+
+          if (hasMilchshake === Availability.NOT_AVAILABLE) {
+            if (newPos.hasMilchshake !== Availability.NOT_AVAILABLE) {
+              newPos.timeSinceBrokenMilchshake = now;
+            }
+          } else {
+            newPos.timeSinceBrokenMilchshake = null;
+          }
+          newPos.hasMilchshake = hasMilchshake;
+
+          if (hasMcFlurry === Availability.NOT_AVAILABLE) {
+            if (newPos.hasMcFlurry !== Availability.NOT_AVAILABLE) {
+              newPos.timeSinceBrokenMcFlurry = now;
+            }
+          } else {
+            newPos.timeSinceBrokenMcFlurry = null;
+          }
+          newPos.hasMcFlurry = hasMcFlurry;
+
+          if (hasMcSundae === Availability.NOT_AVAILABLE) {
+            if (newPos.hasMcSundae !== Availability.NOT_AVAILABLE) {
+              newPos.timeSinceBrokenMcSundae = now;
+            }
+          } else {
+            newPos.timeSinceBrokenMcSundae = null;
+          }
+          newPos.hasMcSundae = hasMcSundae;
+
+          newPos.restaurantStatus = status;
+          newPos.lastCheck = now;
+
+          newPosArray.push(newPos);
+        }),
       );
+    }
 
-      if (status === 'NOT CONNECTED') {
-        return;
-      }
-
-      if (hasMilchshake === Availability.NOT_AVAILABLE) {
-        if (newPos.hasMilchshake !== Availability.NOT_AVAILABLE) {
-          newPos.timeSinceBrokenMilchshake = now;
-        }
-      } else {
-        newPos.timeSinceBrokenMilchshake = null;
-      }
-      newPos.hasMilchshake = hasMilchshake;
-
-      if (hasMcFlurry === Availability.NOT_AVAILABLE) {
-        if (newPos.hasMcFlurry !== Availability.NOT_AVAILABLE) {
-          newPos.timeSinceBrokenMcFlurry = now;
-        }
-      } else {
-        newPos.timeSinceBrokenMcFlurry = null;
-      }
-      newPos.hasMcFlurry = hasMcFlurry;
-
-      if (hasMcSundae === Availability.NOT_AVAILABLE) {
-        if (newPos.hasMcSundae !== Availability.NOT_AVAILABLE) {
-          newPos.timeSinceBrokenMcSundae = now;
-        }
-      } else {
-        newPos.timeSinceBrokenMcSundae = null;
-      }
-      newPos.hasMcSundae = hasMcSundae;
-
-      newPos.restaurantStatus = status;
-      newPos.lastCheck = now;
-
-      newPosArray.push(newPos);
-    }));
+    await Pos.save(newPosArray);
+  } catch (error) {
+    logger.error(error);
   }
-
-  await Pos.save(newPosArray);
 };
