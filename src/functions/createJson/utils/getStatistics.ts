d1b70ca083b146034bb2ed5be8b45b41e49e0a68
paulbtw <paul.vatiche@gmail.com>
2022-01-17 14:36:48 +0100

    fix: add missing trackable column

--- src/functions/createJson/utils/getStatistics.ts
+++ src/functions/createJson/utils/getStatistics.ts
@@ -3,31 +3,34 @@ import { IStatsSQLQueryFormatted, IStatsSQLQueryResult } from '../../../types';
 
 export const getStatistics = async () => {
   const queryResult: IStatsSQLQueryResult[] = await Pos.query(`
-                    SELECT COUNT(*) AS TOTAL,
-                          COUNT(CASE
-                                    WHEN "hasMilchshake" = 'AVAILABLE' THEN 1
-                                END) AS AVAILABLEMILCHSHAKES,
-                          COUNT(CASE
-                                    WHEN "hasMilchshake" NOT IN ('UNKNOWN', 'NOT_APPLICABLE') THEN 1
-                                END) AS TOTALMILCHSHAKES,
-                        MIN("timeSinceBrokenMilchshake") as LONGESTBROKENMILCHSHAKE,
-                          COUNT(CASE
-                                    WHEN "hasMcFlurry" = 'AVAILABLE' THEN 1
-                                END) AS AVAILABLEMCFLURRYS,
-                          COUNT(CASE
-                                    WHEN "hasMcFlurry" NOT IN ('UNKNOWN', 'NOT_APPLICABLE') THEN 1
-                                END) AS TOTALMCFLURRYS,
-                          MIN("timeSinceBrokenMcFlurry") as LONGESTBROKENMCFLURRY,
-                          COUNT(CASE
-                                    WHEN "hasMcSundae" = 'AVAILABLE' THEN 1
-                                END) AS AVAILABLEMCSUNDAES,
-                          COUNT(CASE
-                                    WHEN "hasMcSundae" NOT IN ('UNKNOWN', 'NOT_APPLICABLE') THEN 1
-                                END) AS TOTALMCSUNDAES,
-                          MIN("timeSinceBrokenMcSundae") as LONGESTBROKENMCSUNDAE,
-                          COUNTRY
-                    FROM POS
-                    GROUP BY COUNTRY`);
+                          SELECT COUNT(*) AS TOTAL,
+                                COUNT(CASE
+                                          WHEN "hasMobileOrdering" = TRUE THEN 1
+                                      END) AS TRACKABLE,
+                                COUNT(CASE
+                                          WHEN "hasMilchshake" = 'AVAILABLE' THEN 1
+                                      END) AS AVAILABLEMILCHSHAKES,
+                                COUNT(CASE
+                                          WHEN "hasMilchshake" NOT IN ('UNKNOWN', 'NOT_APPLICABLE') THEN 1
+                                      END) AS TOTALMILCHSHAKES,
+                                MIN("timeSinceBrokenMilchshake") AS LONGESTBROKENMILCHSHAKE,
+                                COUNT(CASE
+                                          WHEN "hasMcFlurry" = 'AVAILABLE' THEN 1
+                                      END) AS AVAILABLEMCFLURRYS,
+                                COUNT(CASE
+                                          WHEN "hasMcFlurry" NOT IN ('UNKNOWN', 'NOT_APPLICABLE') THEN 1
+                                      END) AS TOTALMCFLURRYS,
+                                MIN("timeSinceBrokenMcFlurry") AS LONGESTBROKENMCFLURRY,
+                                COUNT(CASE
+                                          WHEN "hasMcSundae" = 'AVAILABLE' THEN 1
+                                      END) AS AVAILABLEMCSUNDAES,
+                                COUNT(CASE
+                                          WHEN "hasMcSundae" NOT IN ('UNKNOWN', 'NOT_APPLICABLE') THEN 1
+                                      END) AS TOTALMCSUNDAES,
+                                MIN("timeSinceBrokenMcSundae") AS LONGESTBROKENMCSUNDAE,
+                                COUNTRY
+                          FROM POS
+                          GROUP BY COUNTRY`);
 
   return queryResult.map<IStatsSQLQueryFormatted>((result) => ({
     total: parseInt(result.total, 10) || 0,
