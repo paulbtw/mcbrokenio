45782f1e3c32514327d0f78b1ae2d56f3f81fff7
paulbtw <paul.vatiche@gmail.com>
2022-01-25 13:48:56 +0100

    fix: remove unused code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,5 @@ export * from './createDatabaseConnection';
 export * from './delay';
 export * from './getClientId';
 export * from './getNewBearerToken';
-export * from './getLoginBearerToken';
 export * from './CountryInfos';
 export * from './chunk';
