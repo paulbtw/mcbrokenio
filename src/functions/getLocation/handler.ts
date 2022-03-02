0d24ad648302e7f37d20e84644c194c4e76527a4
paulbtw <paul.vatiche@gmail.com>
2022-03-02 13:37:37 +0100

    feat: add location lambda

---
+++ src/functions/getLocation/handler.ts
@@ -0,0 +1,38 @@
+import { Logger } from '@sailplane/logger';
+import { APIGatewayEvent, Handler } from 'aws-lambda';
+import axios from 'axios';
+
+const logger = new Logger('getLocation');
+
+export interface IIPService {
+  query: string;
+  status: string;
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+}
+
+export const main: Handler<APIGatewayEvent> = async (event) => {
+  const ip = event.headers['x-forwarded-for'];
+  logger.debugObject('event', ip);
+
+  const location = await axios.get<IIPService>(`http://ip-api.com/json/${ip}`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      success: true,
+      lat: location.data.lat,
+      lon: location.data.lon,
+    }),
+  };
+};
