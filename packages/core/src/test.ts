41ef1a38e5b3eb8724bcee186d070a4aaa5df2f1
paulbtw <paul.vatiche@gmail.com>
2023-07-29 23:20:25 +0200

    chore: tryout nx :)

--- packages/core/src/test.ts
+++
@@ -1,3 +0,0 @@
-export function RandomFunction() {
-  console.log('test');
-}
--- packages/mcbroken/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { RandomFunction } from '@mcb/core/test';
-
-export const run = () => {
-  const time = new Date();
-  console.log(`Your cron function ran at ${time}`);
-  RandomFunction();
-};
