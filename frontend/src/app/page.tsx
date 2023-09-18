6cb70f7981e650719e3f53795a11dd064ac452f8
paulbtw <paul.vatiche@gmail.com>
2023-09-18 21:07:49 +0200

    feat: add content container

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,11 +2,11 @@ import { Map } from '@/components/Map'
 
 export default function Home() {
   return (
-    <main className="h-screen max-w-5xl p-5 flex items-center flex-col grow">
+    <div className="w-full box-border">
       <div>McBroken</div>
-      <div className="w-full h-[70vh]">
+      <div className="w-full h-[70vh] rounded-xl overflow-hidden">
         <Map />
       </div>
-    </main>
+    </div>
   )
 }
