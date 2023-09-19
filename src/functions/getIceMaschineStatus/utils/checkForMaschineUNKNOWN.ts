54ade8eb6a72f378da32c7d88e84a0e842b518d5
Paul Vatiche <paul@vatiche.de>
2023-09-19 23:41:58 +0200

    fix: remove old code

--- src/functions/getIceMaschineStatus/utils/checkForMaschineUNKNOWN.ts
+++
@@ -1,8 +0,0 @@
-import { Availability } from '../../../types';
-
-export const checkForMaschineUNKNOWN = async () => ({
-  hasMilchshake: Availability.UNKNOWN,
-  hasMcSundae: Availability.UNKNOWN,
-  hasMcFlurry: Availability.UNKNOWN,
-  status: 'UNKNOWN',
-});
