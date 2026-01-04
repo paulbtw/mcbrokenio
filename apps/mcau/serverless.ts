98e513c74be44545f45842bf48c3d428ccb712ba
paulbtw <paul.vatiche@gmail.com>
2026-01-04 21:32:00 +0100

    fix: eslint missing

--- apps/mcau/serverless.ts
+++ apps/mcau/serverless.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-restricted-imports */
 import { baseServerlessConfiguration } from '@mcbroken/serverless-config'
 import type { AWS } from '@serverless/typescript'
 
